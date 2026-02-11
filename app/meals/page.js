61a652bc4ee830aad17cfe8c6a8795782e9a4fb8
Say.vi <say.julia.vi@gmail.com>
2026-02-11 02:18:58 +0100

    meals page and sql

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import Link from 'next/link';
 import MealsGrid from '../../components/meals/meal-grid';
+import { getMeals } from '@/lib/meals';
+import classes from './page.module.css';
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
-      <header className="header">
+      <header className={classes.header}>
         <h1>
           Delicious meals, created{' '}
-          <span className="highlight">by you</span>
+          <span className={classes.highlight}>by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
-        <p className="cta">
-          <a href="/meals/share">
+        <p className={classes.cta}>
+          <Link href="/meals/share">
             Share Your Favorite Recipe
-          </a>
+          </Link>
         </p>
       </header>
-      <main className="main">
-        <MealsGrid meals={[]} />
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
